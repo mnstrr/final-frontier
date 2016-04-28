c4881b0c06e8c3e79bd10aedf49dfaea42e13b9e
TrangD <trang.d.wiredelta@gmail.com>
2016-04-28 15:42:17 +0200

    remove .idea

--- crawler.py
+++ crawler.py
@@ -1,34 +1,35 @@
 import urllib.request
-#from sets import Set
 from bs4 import BeautifulSoup as bs
-import collections
 
 baseURL = 'http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/'
 
-seedList = ['http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d01.html','http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d06.html','http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d08.html']
+seedList = ['http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d01.html',
+            'http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d06.html',
+            'http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d08.html']
 frontierList = []
 visitedList = set()
 
-def main ():
-	for url in seedList:
 
-		frontierList.append(url)
+def main():
+    for url in seedList:
 
-		while len(frontierList) > 0:
-			currentURL = frontierList[0]
-			page = urllib.request.urlopen(currentURL)
-			soup = bs(page.read(), "html.parser")
-			visitedList.add(currentURL)
-			frontierList.pop()[0]
+        frontierList.append(url)
 
-			for link in soup.find_all('a'):
-				url1 = baseURL + link.get('href')
-				if url1 not in visitedList:
-					frontierList.append(url1)
-					visitedList.add(url1)
+        while len(frontierList) > 0:
+            currentURL = frontierList[0]
+            page = urllib.request.urlopen(currentURL)
+            soup = bs(page.read(), "html.parser")
+            visitedList.add(currentURL)
+            frontierList.pop()[0]
 
-	for ele in visitedList:
-		print(ele)
+            for link in soup.find_all('a'):
+                url1 = baseURL + link.get('href')
+                if url1 not in visitedList:
+                    frontierList.append(url1)
+                    visitedList.add(url1)
+
+    for ele in visitedList:
+        print(ele)
 
 
 main()
