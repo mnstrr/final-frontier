87c9e2cc4603bcef5b498077fa442e726a5f5336
Felix Winkelmann <felixwinkelmann@hotmail.com>
2016-05-02 18:58:52 +0200

    split crawler in class, add main class

--- crawler.py
+++ crawler.py
@@ -1,33 +1,33 @@
 import urllib.request
-#from sets import Set
 from bs4 import BeautifulSoup as bs
-import collections
 
-baseURL = 'http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/'
 
-seedList = ['http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d01.html','http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d06.html','http://home.htw-berlin.de/~iclassen/cmst/ue1/docs/d08.html']
-frontierList = []
-visitedList = set()
+class Crawler:
+    def __init__(self, seedList, baseUrl):
+        self.__seedList = seedList
+        self.__baseUrl = baseUrl
+        self.__frontierList = []
+        self.__visitedList = set()
+        self.__crawl()
 
-def main ():
-	for url in seedList:
+    def __crawl(self):
+        for url in self.__seedList:
 
-		frontierList.append(url)
+            self.__frontierList.append(url)
 
-		while len(frontierList) > 0:
-			currentURL = frontierList[0]
-			page = urllib.request.urlopen(currentURL)
-			soup = bs(page.read(), "html.parser")
-			visitedList.add(currentURL)
-			frontierList.pop()[0]
+            while len(self.__frontierList) > 0:
+                currenturl = self.__frontierList[0]
+                page = urllib.request.urlopen(currenturl)
+                soup = bs(page.read(), "html.parser")
+                self.__visitedList.add(currenturl)
+                self.__frontierList.pop()[0]
 
-			for link in soup.find_all('a'):
-				url1 = baseURL + link.get('href')
-				if url1 not in visitedList:
-					frontierList.append(url1)
-					visitedList.add(url1)
+                for link in soup.find_all('a'):
+                    url1 = self.__baseUrl + link.get('href')
+                    if url1 not in self.__visitedList:
+                        self.__frontierList.append(url1)
+                        self.__visitedList.add(url1)
 
-	for ele in visitedList:
-		print(ele)
-
-main()
+    def printLinks(self):
+        for ele in self.__visitedList:
+            print(ele)
