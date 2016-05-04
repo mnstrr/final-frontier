db9df31f79572625c41c1f53da51c019a76e06db
TrangD <trang.d.wiredelta@gmail.com>
2016-05-04 23:32:40 +0200

    refactor variable names, debug pop()

--- crawler.py
+++ crawler.py
@@ -5,28 +5,28 @@ class Crawler:
     def __init__(self, seedList, baseUrl):
         self.__seedList = seedList
         self.__baseUrl = baseUrl
-        self.__frontierList = []
-        self.__visitedList = set()
+        self.__frontier = list()
+        self.__visited = set()
         self.__crawl()
 
     def __crawl(self):
         for url in self.__seedList:
 
-            self.__frontierList.append(url)
+            self.__frontier.append(self.__baseUrl + url)
 
-            while len(self.__frontierList) > 0:
-                currenturl = self.__frontierList[0]
-                page = urllib.request.urlopen(currenturl)
+            while len(self.__frontier) > 0:
+                currentURL = self.__frontier[0]
+                page = urllib.request.urlopen(currentURL)
                 soup = bs(page.read(), "html.parser")
-                self.__visitedList.add(currenturl)
-                self.__frontierList.pop()[0]
+                self.__visited.add(currentURL)
+                self.__frontier.pop(0)
 
-                for link in soup.find_all('a'):
-                    url1 = self.__baseUrl + link.get('href')
-                    if url1 not in self.__visitedList:
-                        self.__frontierList.append(url1)
-                        self.__visitedList.add(url1)
+                for outURL in soup.find_all('a'):
+                    currentOutURL = self.__baseUrl + outURL.get('href')
+                    if currentOutURL not in self.__visited:
+                        self.__frontier.append(currentOutURL)
+                        self.__visited.add(currentOutURL)
 
-    def printLinks(self):
-        for ele in self.__visitedList:
+    def printURLs(self):
+        for ele in self.__visited:
             print(ele)
