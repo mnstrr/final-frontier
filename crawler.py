a287706b67b1931c5da4cc8fc51fc363ad2656a7
paulahaertel <paula.haertel@mailbox.org>
2016-05-11 10:11:09 +0200

    intents building a dict based on the links between pages

--- crawler.py
+++ crawler.py
@@ -9,6 +9,7 @@ class Crawler:
         self.__baseUrl = baseUrl
         self.__frontierList = []
         self.__visitedList = set()
+        self.__linkList = {}
         self.__crawl()
         self.__test()
 
@@ -19,20 +20,41 @@ class Crawler:
 
             while len(self.__frontierList) > 0:
                 currenturl = self.__frontierList[0]
+                self.__pagerank(currenturl)
                 page = ur.urlopen(currenturl)
                 soup = bs(page.read(), "html.parser")
                 self.__visitedList.add(currenturl)
                 self.__frontierList.pop()[0]
 
+                curr_url_list = []
+
                 for link in soup.find_all('a'):
                     url1 = self.__baseUrl + link.get('href')
+
+                    # add url1 to [K:currenturl, V: list/array of urls]
+                    curr_url_list.append(url1)
+
                     if url1 not in self.__visitedList:
                         self.__frontierList.append(url1)
                         self.__visitedList.add(url1)
 
+                mini_dict = {currenturl: curr_url_list}
+                self.__linkList.update(mini_dict)
+
+
+
+
+        print("linkList")
+        print(self.__linkList)
+
     def __test(self):
         m = np.matrix('1 2; 3 4')
         print(m)
+
+    def __pagerank(self, currenturl):
+        cu = currenturl
+        print("currenturl:")
+        print(cu)
 
     def printLinks(self):
         for ele in self.__visitedList:
